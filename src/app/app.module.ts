4d400b8960529e682a0951fb6b7423fc1b3313fc
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2025-05-12 21:34:01 +0100

    customers commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { CustomersComponent } from './customers/customers.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AccountsComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
