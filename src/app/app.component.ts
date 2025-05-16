dbbece1b1f732838d3ea44020dd569fb1f9f2cfa
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2025-05-16 12:45:40 +0100

    service commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'digital-banking-wb';
+  title = 'digital-banking-web';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AccountsComponent } from './accounts/accounts.component';
 import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
 import {HttpClientModule} from "@angular/common/http";
-import { ModelComponent } from './model/model.component';
+import {ReactiveFormsModule} from "@angular/forms";
 import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { CustomerAccountsComponent } from './customer-accounts/customer-accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AccountsComponent,
     CustomersComponent,
-    ModelComponent,
-    NewCustomerComponent
+    AccountsComponent,
+    NewCustomerComponent,
+    CustomerAccountsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
